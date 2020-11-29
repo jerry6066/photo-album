6f633edc4a6e69d8eafbaea516ee52681dce6cbd
Libo Jian <1220266538@qq.com>
2020-11-29 01:21:56 -0500

    commit

--- upload.js
+++ upload.js
@@ -28,3 +28,32 @@ $('#theButton').click(function() {
         })
     });
 });
+
+$('#search').click(function() {
+  var data = {'message': $('#transcript')[0].value}
+  console.log(data)
+  $.ajax({
+    type: 'POST',
+    url: 'https://1hceq7jgh7.execute-api.us-east-1.amazonaws.com/v0/search',
+    crossDomain: true,
+    headers: {'Content-Type': 'application/json'},
+    data: JSON.stringify(data),
+    dataType: "json"
+  })
+  .done(function(response){
+    $('img').addClass('hidden')
+    image_arr = response.imagePaths;
+    if (image_arr.length > 0) {
+      $('#image1').attr('src', image_arr[0]);
+      $('#image1').removeClass('hidden')
+    };
+    if (image_arr.length > 1) {
+      $('#image2').attr('src', image_arr[1]);
+      $('#image2').removeClass('hidden')
+    };
+    if (image_arr.length > 2) {
+      $('#image3').attr('src', image_arr[2]);
+      $('#image3').removeClass('hidden')
+    }
+  })
+})
